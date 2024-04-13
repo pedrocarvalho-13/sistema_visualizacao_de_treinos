ca6f4fcd69aa8ff449621e43b20b5b6f12241639
pedrocarvalho-13 <you@example.compedrogui.carvalho13@gmail.com>
2024-04-13 19:46:12 -0300

    implementação da main

---
+++ main.py
@@ -0,0 +1,13 @@
+from os import system
+
+from modulos.treinoFuctions import visualizar, sair
+
+
+system("cls")
+
+print ('Bem vindo ao seu portal de treino!')
+
+while True:
+    visualizar()
+    sair()
+    break
